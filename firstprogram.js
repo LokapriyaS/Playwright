1f90a8e2421df7070c91591bad1ed09f0b7a6841
LokapriyaS <lokapriyas1926@gmail.com>
2026-08-29 23:44:39 +0530

    Add files via upload

---
+++ firstprogram.js
@@ -0,0 +1,11 @@
+console.log("This is the first program")
+let firstName="Lokapriya";
+console.log(typeof firstName);
+let companyName="Infosys";
+console.log(typeof companyName);
+let mobileNumber=9445006930;
+console.log(typeof mobileNumber);
+let isAutomation=true;
+console.log(typeof isAutomation);
+let hasPlaywright;
+console.log(typeof hasPlaywright);
